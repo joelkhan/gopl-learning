a3db6e2c36d3460c5d28c1ec7448f68428b37bd4
joel khan of work dev <joel-work-dev@khanins.com.cn>
2021-09-18 14:16:21 +0800

    init gopl-learning

---
+++ ch1/helloworld/main.go
@@ -0,0 +1,17 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 1.
+
+// Helloworld is our first Go program.
+//!+
+package main
+
+import "fmt"
+
+// 函数声明
+func main() {
+	fmt.Println("Hello, 世界")
+}
+
+//!-
